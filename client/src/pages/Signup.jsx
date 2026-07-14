2256910e2786777703e51c84fd8c1fa69d3582f8
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-07-14 17:19:18 +0530

    Signup Page

--- client/src/pages/Signup.jsx
+++ client/src/pages/Signup.jsx
@@ -1,6 +1,10 @@
 import {useState} from "react";
+import {useNavigate} from "react-router-dom";
+import api from "../services/api";
 
 function Signup(){
+    const navigate = useNavigate();
+    
     const[formData , setFormData] = useState({
         name : "",
         email : "" ,
@@ -18,13 +22,28 @@ function Signup(){
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // normally when a form is submitted , the browser refreshes the page., ubt we don;t want that to happen , so we stop the default browser behaviour.
+        // normally when a form is submitted , the browser refreshes the page., but we don;t want that to happen , so we stop the default browser behaviour.
 
-        console.log(formData);
 
         if(formData.confirmPassword!==formData.password){
             alert("Passwords do not match");
             return ;
+        }
+
+        try{
+            const response = await api.post(
+                "/auth/signup" , formData);
+                if(response.data.success){
+                    alert(response.data.message);
+                }
+                navigate("/login");
+        } catch (error) {
+            alert(error.response?.data?.message || "somewthing went wrong");
+
+
+            // we could hv written alert(error.response.data.message) but we wrote this becuase maybe server is down in that case something has to be displayed due to which we wrote it.
+
+            // ?. = optional chaining which means it this exists continue , if it doesn;t exist, stop and return undefined instead of crashing.
         }
     }
     return (
@@ -33,13 +52,12 @@ function Signup(){
 
             <input type="text" name="name" placeholder="Enter your name" value={formData.name} onChange={handleChange}/>
 
-            <input type="text" name="email" placeholder="Enter your email" value={formData.email} onChange={handleChange}/>
+            <input type="email" name="email" placeholder="Enter your email" value={formData.email} onChange={handleChange}/>
 
-            <input type="text" name="password" placeholder="Enter your password" value={formData.password} onChange={handleChange}/>
+            <input type="password" name="password" placeholder="Enter your password" value={formData.password} onChange={handleChange}/>
 
-            <input type="text" name="confirmPassword" placeholder="Confirm your password" value={formData.confirmPassword} onChange={handleChange}/>
+            <input type="password" name="confirmPassword" placeholder="Confirm your password" value={formData.confirmPassword} onChange={handleChange}/>
 
-            <button>Create Account</button>
             <button type="submit">Create Account</button>
         </form>
     );
