3f46378463347d5bb3ab9a026372ad47e456f95c
iamjahnvi <iamjahnvidwivedi@gmail.com>
2026-08-01 23:24:54 +0530

    Code for status of exam

--- client/src/pages/ExamDetails.jsx
+++ client/src/pages/ExamDetails.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { useParams , useNavigate } from "react-router-dom";
 
 // useParams is a hook from react-router-dom , that allows the component i.e ExamDetails.jsx to access dynamic segments of the URL
 // for eg : if ur route is /exam/:id , then useParams will give us the value of id.
@@ -7,13 +7,21 @@ import { useEffect, useState } from "react";
 
 import api from "../services/api";
 
-
 function ExamDetails() {
+
+    const navigate = useNavigate();
 
     const { id } = useParams();
     // useParams extracts the dyanmic value from the url 
 
     const[exam , setExam] = useState(null);
+
+    const getRegistrationStatus = (start , end ) => {
+        const today = new Date();
+        if(today < new Date(start)) return "Upcoming" ;
+        if(today >= new Date(end)) return "Open"   ;
+        return "Closed"; 
+    }
 
     useEffect(() => {
 
@@ -22,7 +30,8 @@ function ExamDetails() {
 
         const fetchExam = async () => {
             try{
-                const response = await api.get(`/exams/${id}`)
+                const response = await api.get(`/exams/${id}`);
+                console.log(response.data.data);
                 if(response.data.success){
                     setExam(response.data.data);
                 }
@@ -47,15 +56,56 @@ function ExamDetails() {
             <p>{exam.fullForm}</p>
             <p>{exam.description}</p>
 
-            <p>Minimum Age : {exam.minimumAge}</p>
+            <h3>Eligibility</h3>
+
+            <p>Minimum Age : {exam.minimumAge ?? "No age limit"}</p>
 
             <p>Stream : {exam.streams?.join(" , "
             )}</p>
 
-            <p>Registration Status : {exam.registrationStatus}</p>
+            <p>Minimum Education Level : {exam.minimumEducationLevel}</p>
 
-            <button onClick = {() => 
-                window.open(exam.officialWebsite , "_blank")}>Visit official website</button>
+            <p>Minimum Percentage : {exam.eligibility?.minimumPercentage ?? "Not specified"}%</p>
+
+            <p>Subjects : {exam.subjects?.join(",")}</p>
+
+            <button onClick={() => {
+                window.open(exam.officialWebsite,"_blank")
+            }}>
+                Visit officialWebsite
+            </button>
+
+            <hr />
+
+            <h2>Registration Details </h2>
+
+            <p>
+                <strong>Start Date : </strong>{" "}
+                {new Date(exam.registrationStartDate).toLocaleDateString("en-IN" , {
+                    day : "numeric" ,
+                    month : "long" ,
+                    year : "numeric",
+                })}
+            </p>
+
+            <p>
+                <strong>End Date : </strong>{" "}
+                {new Date(exam.registrationEndDate).toLocaleDateString()};
+            </p>
+
+            {/* {""} = inserts a forced blank space between the bold label and the date value so that they don't run together on the screen */}
+
+            {/* new Date(...) = converts the raw data into proper js Date Object */}
+
+            {/* strong = making text appear bold */}
+
+
+            <p>
+                <strong>Registration Status</strong>{" "}
+                {getRegistrationStatus(exam.registrationStartDate , exam.registrationEndDate)}
+            </p>
+
+            <button onClick={()=> navigate("/recommendations")}>Back to recommendations</button>
         </div>
     )
 
